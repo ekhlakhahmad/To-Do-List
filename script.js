a0ffafe24910ea37412900d95f03900542997388
Ekhlakh Ahmad <ekhlakhahmad95@gmail.com>
2024-06-06 19:04:04 +0530

    some changed

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-const Dark = document.getElementById("darkLight");
-const body = document.querySelector("body");
+let darkBg = document.querySelector(".darkBg");
+let blueBg = document.querySelector(".blueBg");
+let greenBg = document.querySelector(".greenBg");
+let pinkBg = document.querySelector(".pinkBg");
+let lightBg = document.querySelector(".lightBg");
+let yellowBg = document.querySelector(".yellowBg");
 
-let currMode = "light";
-
-Dark.addEventListener("click", function (e) {
-  if (currMode === "light") {
-    currMode = "dark";
-    body.style.backgroundColor = "#000000";
-  } else {
-    currMode = "light";
-    body.style.backgroundColor = "#ffffff";
-  }
-});
+let body = document.querySelector(".bodyBg");
 
 let inputField = document.getElementById("inputBox");
 let addBtn = document.getElementById("addBtn");
 let listFields = document.getElementById("listFields");
+
+darkBg.addEventListener("click", () => {
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+  listFields.style.color = "black";
+})
+blueBg.addEventListener("click", () => {
+  body.style.backgroundColor = "blue";
+  body.style.color = "white";
+  listFields.style.color = "black";
+})
+greenBg.addEventListener("click", () => {
+  body.style.backgroundColor = "green";
+  body.style.color = "white";
+  listFields.style.color = "black";
+})
+pinkBg.addEventListener("click", () => {
+  body.style.backgroundColor = "pink";
+  body.style.color = "black";
+})
+yellowBg.addEventListener("click", () => {
+  body.style.backgroundColor = "yellow";
+  body.style.color = "black";
+})
+lightBg.addEventListener("click", () => {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+})
 
 const AddTask = () => {
   if (inputField.value == "") {
